cf14f3ca841cbc9e2b999df67251d16287b4614a
정양효 <jyhyo2002@gmail.com>
2024-03-14 17:34:23 +0900

    0314 과제제출

--- 1부터 100까지 소수의 합.c
+++ 1부터 100까지 소수의 합.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
 	int result = 0;
 	int n = 0;
-	for (int i = 2; i <= 100; i++)
+	clock_t start, stop;
+	double duration;
+	start = clock();
+	for (int i = 2; i <= 100; i++) 
 	{
 		for (n = 2; n < i; n++)
 		{
-			if (i % n == 0) break;
+			if (i % n == 0) 
+				break;
 		}
 		if (n == i)
-			result += n;
+			result += i;
+
 	}
+	stop = clock();
+	duration = (double)(stop - start) / CLOCKS_PER_SEC;
+	printf("수행시간은 %f초 입니다.", duration);
 	printf("1부터 100까지 소수의 합 = %d\n", result);
-	system("pause");
+	return 0;
+	
 }
